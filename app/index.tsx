3cc6cff67042a1a2e776d16acf10a5d64906055f
carolinevannebo <carolinevannebo@icloud.com>
2024-11-14 20:25:14 +0100

    fixing white statusbar for browser

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { HoldTheDate, ScreenTemplate } from "@/components";
+import { Assets } from "@/Assets";
 
 const Index = () => {
 
   return (
       <ScreenTemplate>
         <HoldTheDate />
-        <StatusBar style="auto" />
+        <StatusBar style="dark" backgroundColor={Assets.colors.gradient.medium} />
       </ScreenTemplate>
   );
 }
