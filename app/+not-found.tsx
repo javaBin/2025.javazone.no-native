801d62f5c36ec3007085b6ffbd3268ef353f4dea
carolinevannebo <carolinevannebo@icloud.com>
2024-11-15 18:04:12 +0100

    fixed styling issues for 404

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,13 +1,56 @@
 // @ts-ignore
 import { ScreenTemplate } from "@/components";
 import { useTranslation } from "react-i18next";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
 // @ts-ignore
 import { Assets } from "@/Assets";
 import { Link } from "expo-router";
 
 const NotFound = () => {
     const { t } = useTranslation();
+    const screenWidth = Dimensions.get("window").width;
+
+    const styles = StyleSheet.create({
+        content: {
+            display: 'flex',
+            justifyContent: "center",
+            alignItems: "center",
+            width: '100%',
+        },
+        errorCode: {
+            color: Assets.colors.brand.cream,
+            fontSize: Platform.OS == 'web' ? 58 : 44,
+            fontWeight: '800',
+            textTransform: 'uppercase',
+        },
+        errorTitle: {
+            color: Assets.colors.logo.brightOrange,
+            width: '95%',
+            fontSize: screenWidth > 768 ? 36 : 32,
+            fontWeight: 'semibold',
+            marginBottom: 20,
+            textAlign: 'center',
+        },
+        errorMessageLight: {
+            color: Assets.colors.brand.dutchWhite
+        },
+        errorMessageMedium: {
+            color: Assets.colors.brand.beige,
+        },
+        text: {
+            margin: 10,
+            width: screenWidth > 768 ? '60%' : '85%',
+            fontSize: screenWidth > 768 ? 20 : 18,
+        },
+        highlight: {
+            color: Assets.colors.logo.brightRed,
+            fontWeight: 'bold'
+        },
+        link: {
+            color: Assets.colors.logo.brightYellow,
+            textDecorationLine: 'underline',
+        }
+    });
 
     return (
         <ScreenTemplate>
@@ -34,46 +77,5 @@ const NotFound = () => {
     )
 }
 
-const styles = StyleSheet.create({
-    content: {
-        display: 'flex',
-        justifyContent: "center",
-        alignItems: "center",
-        width: '100%',
-    },
-    errorCode: {
-        color: Assets.colors.brand.cream,
-        fontSize: Platform.OS == 'web' ? 58 : 44,
-        fontWeight: '800',
-        textTransform: 'uppercase',
-    },
-    errorTitle: {
-        color: Assets.colors.logo.brightOrange,
-        width: Platform.OS == 'web' ? '70%' : '95%',
-        fontSize: Platform.OS == 'web' ? 36 : 32,
-        fontWeight: 'semibold',
-        marginBottom: 20,
-        textAlign: 'center',
-    },
-    errorMessageLight: {
-        color: Assets.colors.brand.dutchWhite
-    },
-    errorMessageMedium: {
-        color: Assets.colors.brand.beige,
-    },
-    text: {
-        margin: 10,
-        width: Platform.OS == 'web' ? '60%' : '85%',
-        fontSize: Platform.OS == 'web' ? 20 : 18,
-    },
-    highlight: {
-        color: Assets.colors.logo.brightRed,
-        fontWeight: 'bold'
-    },
-    link: {
-        color: Assets.colors.logo.brightYellow,
-        textDecorationLine: 'underline',
-    }
-});
 
 export default NotFound;
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,9 @@
 import { HoldTheDate, ScreenTemplate } from "@/components";
-import {Link} from "expo-router";
 
 const Index = () => {
     return (
           <ScreenTemplate>
             <HoldTheDate />
-              <Link href="/hhh" style={{color: "#ffffff"}}>test</Link>
         </ScreenTemplate>
     );
 }
