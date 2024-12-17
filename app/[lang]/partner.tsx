f723ed29f369f67d675b55121b2fcf4a838165c0
Ellaoui <abab010@student.kristiania.no>
2024-12-17 18:34:01 +0100

    Small test

--- app/[lang]/partner.tsx
+++ app/[lang]/partner.tsx
@@ -10,83 +10,7 @@ const Partner = () => {
 
   return (
     <ScreenTemplate>
-      <ScrollView>
-        <View style={styles.partnerHeadingInfo}>
-          <h1>{t('partner.partner_info.javaZone_2025_partner')}</h1>
-          <p>{t('partner.partner_info.partner_registration_opens')}</p>
-          <p>
-            {t('partner.partner_info.questions_email')}{' '}
-            <a style={{ color: 'inherit' }} href="mailto:partner@java.no">
-              partner@java.no
-            </a>
-          </p>
-        </View>
-
-        <View style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: 50 }}>
-          <h2>{t('partner.partner_info.partner_packages')}</h2>
-          <View style={[styles.partnerContentInfo, isMobile && styles.partnerContentInfoMobile]}>
-            <View style={[styles.partnerContentItem, isMobile && styles.partnerContentItemMobile]}>
-              <h3>{t('partner.partner_info.standard_package')}</h3>
-              <div>
-                <p>{t('partner.partner_info.price')}: 100 000,- NOK ex VAT</p>
-              </div>
-              <ul>
-                <li>{t('partner.partner_info.standard_package_detail_1')}</li>
-                <li>{t('partner.partner_info.standard_package_detail_2')}</li>
-                <li>{t('partner.partner_info.standard_package_detail_3')}</li>
-                <li>{t('partner.partner_info.standard_package_detail_4')}</li>
-                <li>{t('partner.partner_info.standard_package_detail_5')}</li>
-                <li>{t('partner.partner_info.standard_package_detail_6')}</li>
-                <li>{t('partner.partner_info.standard_package_detail_7')}</li>
-              </ul>
-            </View>
-
-            <View style={[styles.partnerContentItem, isMobile && styles.partnerContentItemMobile]}>
-              <h3>{t('partner.partner_info.restaurant_stand')}</h3>
-              <div>
-                <p>{t('partner.partner_info.price')}: 190 000,- NOK ex VAT</p>
-              </div>
-              <ul>
-                <li>{t('partner.partner_info.restaurant_stand_detail_1')}</li>
-                <li>{t('partner.partner_info.restaurant_stand_detail_2')}</li>
-                <li>{t('partner.partner_info.restaurant_stand_detail_3')}</li>
-                <li>{t('partner.partner_info.restaurant_stand_detail_4')}</li>
-              </ul>
-            </View>
-
-            <View style={[styles.partnerContentItem, isMobile && styles.partnerContentItemMobile]}>
-              <h3>{t('partner.partner_info.concept_stand')}</h3>
-              <div>
-                <p>{t('partner.partner_info.price')}: 190 000,- NOK ex VAT</p>
-              </div>
-              <ul>
-                <li>{t('partner.partner_info.concept_stand_detail_1')}</li>
-                <li>{t('partner.partner_info.concept_stand_detail_2')}</li>
-                <li>{t('partner.partner_info.concept_stand_detail_3')}</li>
-              </ul>
-            </View>
-
-            <View style={[styles.partnerContentItem, isMobile && styles.partnerContentItemMobile]}>
-              <h3>{t('partner.partner_info.extended_package')}</h3>
-              <p>{t('partner.partner_info.same_as_standard')}</p>
-              <div>
-                <p>{t('partner.partner_info.price')}: 160 000,- NOK ex VAT</p>
-              </div>
-            </View>
-
-            <View style={[styles.partnerContentItem, isMobile && styles.partnerContentItemMobile]}>
-              <h3>{t('partner.partner_info.partner_tickets')}</h3>
-              <p>{t('partner.partner_info.partner_tickets_detail_1')}</p>
-              <div>
-                <p>{t('partner.partner_info.price')}: 9 220,- NOK ex VAT incl ticket fee</p>
-              </div>
-              <ul>
-                <li>{t('partner.partner_info.round_robin_distribution')}</li>
-                <li>{t('partner.partner_info.first_come_first_served')}</li>
-              </ul>
-            </View>
-          </View>
-        </View>
+      <ScrollView style={{ zIndex: -1 }}>
         <View style={styles.content}>
           <Text style={styles.heading}>{t('partner.watch_partner_meeting')}</Text>
           <VideoPlayer videoUrl={'https://player.vimeo.com/video/1038270530'} />
@@ -119,14 +43,11 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   partnerContentItemMobile: {
-    flexBasis: 345,
+    flexBasis: 285,
     padding: 3,
   },
   content: {
     marginTop: 50,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'space-around',
   },
   heading: {
     marginHorizontal: 20,
--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
 
 type VideoPlayerProps = {
   videoUrl: string;
@@ -6,21 +6,19 @@ type VideoPlayerProps = {
 
 const VideoPlayer = ({ videoUrl }: VideoPlayerProps) => {
   const screenWidth = Dimensions.get('window').width;
-  const videoHeight = screenWidth / 3;
+  const videoHeight = screenWidth / 1.9;
 
   return (
-    <>
-      <div style={styles.container}>
-        <iframe
-          src={`${videoUrl}?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`}
-          frameBorder="0"
-          allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-          style={{ ...styles.video, width: screenWidth, height: videoHeight }}
-          title="JavaZone 2025 Partner meeting #1"
-        ></iframe>
-      </div>
+    <View style={styles.container}>
+      <iframe
+        src={`${videoUrl}?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`}
+        frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+        style={{ ...styles.video, width: screenWidth / 1.1, height: 550 }}
+        title="JavaZone 2025 Partner meeting #1"
+      ></iframe>
       <script src="https://player.vimeo.com/api/player.js"></script>
-    </>
+    </View>
   );
 };
 
