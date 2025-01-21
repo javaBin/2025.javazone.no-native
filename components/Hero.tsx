7d9f838698c0160f3f5470b9e19d1c22bdc91e07
HenrikEide <Henrik225@gmail.com>
2025-01-21 18:57:08 +0100

    fix: div -> view

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,15 @@
 import { Assets } from '@/Assets';
 import { SvgImage } from '@/components';
+import { View } from 'react-native';
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
       <SvgImage SVG={Assets.images.hero.duke} height={20} />
       <SvgImage SVG={Assets.images.hero.javaZone} height={20} />
       <SvgImage SVG={Assets.images.hero.divider} height={20} />
       <SvgImage SVG={Assets.images.hero.year} height={20} />
-    </div>
+    </View>
   );
 };
 
