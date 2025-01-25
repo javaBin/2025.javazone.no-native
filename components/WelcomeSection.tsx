cf1673f9a2a24fb3f0ddf07203c4cce4c0e2aa06
Ellaoui <abab010@student.kristiania.no>
2025-01-25 14:23:56 +0100

    Add to language pack

--- components/WelcomeSection.tsx
+++ components/WelcomeSection.tsx
@@ -1,12 +1,13 @@
 import { View, Text, StyleSheet, Platform } from 'react-native';
 import { Assets } from '@/Assets';
+import { useTranslation } from 'react-i18next';
 
 const WelcomeSection = () => {
+  const { t } = useTranslation();
+
   return (
     <View style={[styles.titleContainer, { marginTop: 20, flexDirection: 'column' }]}>
-      <Text style={{ fontFamily: 'PlayfairDisplay_400Regular', fontSize: 30 }}>
-        Velkommen til NOVA Spektrum, Lillestrøm
-      </Text>
+      <Text style={{ fontFamily: 'PlayfairDisplay_400Regular', fontSize: 30 }}>{t('javaZone.welcome_to_NOVA')}</Text>
       <Text style={{ fontFamily: 'PlayfairDisplay_400Regular', fontSize: 30 }}>September 3-4</Text>
     </View>
   );
