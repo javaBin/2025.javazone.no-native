e80e5941a72363dac8cc294cba3abc6a8a31d419
Ellaoui <abab010@student.kristiania.no>
2025-02-05 22:50:11 +0100

    Add ticket sales text and styling

--- components/WelcomeSection.tsx
+++ components/WelcomeSection.tsx
@@ -1,13 +1,32 @@
 import { View, Text, StyleSheet, Platform, Dimensions } from 'react-native';
 import { useTranslation } from 'react-i18next';
+import { BlurView } from 'expo-blur';
+import { useMediaQuery } from 'react-responsive';
+import { SvgImage } from '@/UI';
+import { Assets } from '@/Assets';
 
 const WelcomeSection = () => {
   const { t } = useTranslation();
+  const isMobile = useMediaQuery({ maxWidth: 768 });
 
   return (
     <View style={[styles.titleContainer, { marginTop: 20, flexDirection: 'column' }]}>
       <Text style={styles.welcomeText}>{t('javaZone.welcome_to_NOVA')}</Text>
       <Text style={styles.welcomeText}>September 3-4</Text>
+      <View style={styles.blurViewWrapper}>
+        <SvgImage SVG={Assets.UI.VineLeft} height={100} style={styles.vineLeft} />
+        <BlurView
+          tint="light"
+          intensity={10}
+          style={[
+            { ...styles.ticketContentItem, ...styles.ticketContentItemUniversal },
+            isMobile && styles.ticketContentItemMobile,
+          ]}
+        >
+          <Text style={styles.welcomeText}>Ticket sales will start March 3rd</Text>
+        </BlurView>
+        <SvgImage SVG={Assets.UI.VineRight} height={100} style={styles.vineRight} />
+      </View>
     </View>
   );
 };
@@ -30,5 +49,40 @@ const styles = StyleSheet.create({
     fontFamily: 'PlayfairDisplay_400Regular',
     fontSize: Dimensions.get('window').width > 768 ? 30 : 25,
     textAlign: 'center',
-  }
+  },
+  ticketContentItemUniversal: {
+    marginVertical: 10,
+    borderRadius: 5,
+    shadowColor: '#403532',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: -1, height: 1 },
+    shadowRadius: 10,
+  },
+  ticketContentItem: {
+    alignItems: 'center',
+    flexBasis: 'auto',
+    padding: 25,
+    marginHorizontal: 5,
+    width: Dimensions.get('window').width > 768 ? '100%' : '100%',
+  },
+  ticketContentItemMobile: {
+    flexBasis: 'auto',
+    padding: 10,
+  },
+  blurViewWrapper: {
+    position: 'relative',
+    alignItems: 'center',
+  },
+  vineLeft: {
+    position: 'absolute',
+    left: -5,
+    top: 10,
+    zIndex: 1,
+  },
+  vineRight: {
+    position: 'absolute',
+    right: -5,
+    top: 10,
+    zIndex: 1,
+  },
 });
