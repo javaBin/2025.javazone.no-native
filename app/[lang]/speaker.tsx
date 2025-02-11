2bce81956133a183481decdd16f0cfa42d9457bd
carolinevannebo <carolinevannebo@icloud.com>
2025-02-11 17:47:18 +0100

    template for speaker page

--- app/[lang]/speaker.tsx
+++ app/[lang]/speaker.tsx
@@ -1,12 +1,57 @@
-import { HoldTheDate, ScreenTemplate } from '@/components';
+import { ScreenTemplate } from '@/components';
 import { useTranslation } from 'react-i18next';
+import {SectionBox, SvgImage} from "@/UI";
+import {Text} from "react-native";
+import {Assets} from "@/Assets";
+import React from "react";
 
 const Speaker = () => {
   const { t } = useTranslation();
 
   return (
-    <ScreenTemplate>
-      <HoldTheDate subPageHeader={t('speaker_information')} />
+    <ScreenTemplate pageTitle={t('pageTitles.speaker')} shouldScrollToTop={true}>
+        <SectionBox sectionTitle={"What is it like speaking at JavaZone?"}>
+            <Text style={Assets.styles.text}>lorem ipsum</Text>
+            <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{margin: 10}}/>
+        </SectionBox>
+
+        <SectionBox sectionTitle={"Formats and durations"}>
+            <Text style={Assets.styles.text}>lorem ipsum</Text>
+            <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{margin: 10}}/>
+        </SectionBox>
+
+        <SectionBox sectionTitle={"Audience and topics"}>
+            <Text style={Assets.styles.text}>lorem ipsum</Text>
+            <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{margin: 10}}/>
+        </SectionBox>
+
+        <SectionBox sectionTitle={"Ongoing evaluation of talks"}>
+            <Text style={Assets.styles.text}>lorem ipsum</Text>
+            <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{margin: 10}}/>
+        </SectionBox>
+
+        <SectionBox sectionTitle={"Submission tips"}>
+            <Text style={Assets.styles.text}>lorem ipsum</Text>
+            <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{margin: 10}}/>
+        </SectionBox>
+
+        <SectionBox sectionTitle={"Location"}>
+            <Text style={Assets.styles.text}>lorem ipsum</Text>
+            <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{margin: 10}}/>
+        </SectionBox>
+
+        <SectionBox sectionTitle={"What's in it for me?"}>
+            <Text style={Assets.styles.sectionSubTitle}>Accepted presentations, lightning talks, and workshops</Text>
+            <Text style={Assets.styles.text}>lorem ipsum</Text>
+            <Text style={Assets.styles.sectionSubTitle}>Coverage of expenses</Text>
+            <Text style={Assets.styles.text}>lorem ipsum</Text>
+            <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{margin: 10}}/>
+        </SectionBox>
+
+        <SectionBox sectionTitle={"Important principles"}>
+            <Text style={Assets.styles.text}>lorem ipsum</Text>
+            <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{margin: 10}}/>
+        </SectionBox>
     </ScreenTemplate>
   );
 };
