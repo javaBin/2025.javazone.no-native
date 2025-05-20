c6794114ce99ef4e34989db124a62e50242c73fa
Rina Andriana <rina.andriana@dnb.no>
2025-05-20 20:15:18 +0200

    Started on program page

--- Assets.ts
+++ Assets.ts
@@ -31,6 +31,7 @@ import LinkedInFrame from '@/assets/icons/linkedin-frame.svg';
 import ToTopArrow from '@/assets/icons/to-top-arrow.svg';
 import HandHeartInactive from '@/assets/icons/hand-heart-inactive.svg';
 import HandHeartActive from '@/assets/icons/hand-heart-active.svg';
+import XLogo from '@/assets/icons/x-logo.svg';
 
 // Tab icons
 import Home from '@/assets/icons/home.svg';
@@ -176,6 +177,7 @@ export const Assets = {
     ToTopArrow,
     HandHeartInactive,
     HandHeartActive,
+    XLogo
   },
   partnerLogos: {
     Accenture,
--- app/[lang]/program.tsx
+++ app/[lang]/program.tsx
@@ -1,31 +1,103 @@
-import { HoldTheDate, ScreenTemplate } from '@/components';
+import React, { useEffect } from 'react';
+import { PageTitle, SvgCallbackImage, SvgImage } from '@/UI';
+import { Assets } from '@/Assets';
+import { Dimensions, Linking, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Link } from 'expo-router';
 import { useTranslation } from 'react-i18next';
 import { fetchProgram } from '@/api/fetchProgram';
-import { useEffect, useState } from 'react';
-import { TalksProgram } from '@/api/types/talksProgram';
-import { Text } from 'react-native';
+import { Session } from '@/api/types/talksProgram';
+import { ScreenTemplate } from '@/components';
 
-const Program = () => {
-  const [programs, setPrograms] = useState<TalksProgram>({ sessions: [] });
+type HoldTheDateProps = {
+  subPageHeader?: string;
+};
+
+const HoldTheDate = ({ subPageHeader }: HoldTheDateProps) => {
   const { t } = useTranslation();
 
-  const allPrograms = fetchProgram();
+  const [sessions, setSessions] = React.useState<Session[]>([]);
 
   useEffect(() => {
-    allPrograms
-      .then((data) => {
-        setPrograms(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching program:', error);
-      });
+    fetchProgram().then((data) => setSessions(data.sessions));
   }, []);
 
   return (
     <ScreenTemplate>
-      <HoldTheDate subPageHeader={t('program_information')} />
+      <View style={[{ marginTop: 50 }]}>
+        <SvgCallbackImage SVG={Assets.images.hero.HeroDuke} height={150} />
+
+        <PageTitle title={t('javaZone_2025')} />
+        <View style={styles.cardFlex}>
+          {sessions.map((session) => (
+            <View style={styles.card}>
+              <View style={styles.cardInner}>
+                <Text style={styles.roomText}>{session.room}</Text>
+                <Text style={styles.cardTitle}>{session.title}</Text>
+                {session.speakers.map((speaker) => (
+                  <Text style={styles.speaker}>{speaker.name}</Text>
+                ))}
+                <View style={{}}>
+                  <TouchableOpacity style={{ marginTop: 10 }} onPress={() => Linking.openURL(`https://x.com/${session.speakers[0].twitter}`)}>
+                  <SvgImage style={{}} SVG={Assets.icons.XLogo} height={20} width={20} />
+                </TouchableOpacity>
+                </View>                
+                <Text style={styles.time}>45 min</Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      </View>
     </ScreenTemplate>
   );
 };
 
-export default Program;
+const styles = StyleSheet.create({
+  cardFlex: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 16,
+    justifyContent: 'center',
+  },
+  card: {
+    borderColor: 'red',
+    borderWidth: 4,
+    borderRadius: 10,
+    overflow: 'hidden',
+    fontFamily: 'PlayfairDisplay_400Regular',
+    maxWidth: 400,
+    width: '100%',
+  },
+  roomText: {
+    color: '#343434',
+    fontSize: 14,
+    marginBottom: 10,
+  },
+  cardTitle: {
+    fontSize: 16,
+    marginBottom: 8,
+    fontWeight: '600',
+  },
+  speaker: {
+    color: '#363636',
+    fontSize: 14,
+  },
+  time: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  cardInner: {
+    borderColor: Assets.colors.jz2025ThemeColors.cyberYellow,
+    backgroundColor: 'white',
+    borderWidth: 4,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    height: '100%',
+  },
+  text: {
+    color: Assets.colors.brand.neutral,
+    fontSize: Platform.OS == 'web' ? 18 : 16,
+  },
+});
+
+export default HoldTheDate;
