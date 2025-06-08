4961a084a7a394c28eb1200dd2e3271b3518ec1c
ellaoui <abdulquddous.abdellaoui@toll.no>
2025-06-08 18:43:51 +0200

    Add english text and use translation

--- app/[lang]/speaker/kids.tsx
+++ app/[lang]/speaker/kids.tsx
@@ -9,45 +9,47 @@ import LinkButton from '@/UI/LinkButton';
 
 const workshops = [
   {
-    title: 'BitBot XL - Selvkjørende bil',
-    room: 'Ada',
-    instructors: ['Bjørn Hamre', 'Thomas Nøkleby', 'Børge Nese'],
-    checkInLink: ',',
-    description: [
-      'Bli med og utforsk bilen BitBot XL. På denne workshopen skal du lære å programmere bilens sensorer slik at den blir selvkjørende. Du skal blant annet lære å bruke avstandssensoren slik at bilen stopper selv og unngår å krasje i omgivelsene.',
-      'Til slutt skal du programmere bilen til å følge en svart linje langs en bane vi tegner på gulvet. Hvor fort klarer du å få bilen til å kjøre, men likevel klare svingene? Kom og finn ut!',
+    title: 'kids.workshop_bitbot_title',
+    room: 'kids.workshop_bitbot_room',
+    instructors: [
+      'kids.workshop_bitbot_instructor_one',
+      'kids.workshop_bitbot_instructor_two',
+      'kids.workshop_bitbot_instructor_three',
     ],
-    info: ['Maks deltagere: 20', 'Alder: 12 – 17 år', 'Språk: Norsk'],
+    checkInLink: 'kids.workshop_bitbot_check_in_link',
+    description: ['kids.workshop_bitbot_description_one', 'kids.workshop_bitbot_description_two'],
+    info: ['kids.workshop_bitbot_info_one', 'kids.workshop_bitbot_info_two', 'kids.workshop_bitbot_info_three'],
     equipment: [
-      'Egen PC eller Mac',
-      'USB-C til USB-A overgang hvis datamaskinen ikke har USB-A-port',
-      'Hvis du har Mac anbefaler vi å bruke Chrome som nettleser',
+      'kids.workshop_bitbot_equipment_one',
+      'kids.workshop_bitbot_equipment_two',
+      'kids.workshop_bitbot_equipment_three',
     ],
   },
   {
-    title: 'Er du klar for å bygge din egen robot?',
-    room: 'Exposalen',
-    instructors: ['Jeroen Resoort', 'Michel Breevoort'],
-    checkInLink: ',',
+    title: 'kids.workshop_mBot_title',
+    room: 'kids.workshop_mBot_room',
+    instructors: ['kids.workshop_mBot_instructor_one', 'kids.workshop_mBot_instructor_two'],
+    checkInLink: 'kids.workshop_mBot_check_in_link',
     description: [
-      'MBot er en kun liten robot som du får kontrollere med en pc! I denne workshopen viser vi hvordan du kan sette den sammen og få den i gang, ved å bruke Python, et programmeringsspråk som er veldig bra og gøy! Du vil få roboten til å blinke med fargerike lys, lage gøyale lyder, og kjøre rundt som en racerbil.',
-      'Først vil du lære det grunnleggende ved Python, og steg for steg programmere din mBot til å gjøre mer og mer. Ditt mål? Bygg en robot som kan kjøre selv og unngå hindringer som profesjonell!',
-      'På veien kan du gjøre sideoppgaver og la kreativiteten gå løpsk. Vi har sett roboter som politi, racerbiler og til og med wrestlingkamper i tidligere workshoper!',
+      'kids.workshop_mBot_description_one',
+      'kids.workshop_mBot_description_two',
+      'kids.workshop_mBot_description_three',
     ],
-    info: ['Maks deltagere: 30', 'Alder: 12 - 17 år', 'Språk: Engelsk'],
-    equipment: ['Egen PC med Windows 10+, Linux, eller MacOS'],
+    info: ['kids.workshop_mBot_info_one', 'kids.workshop_mBot_info_two', 'kids.workshop_mBot_info_three'],
+    equipment: ['kids.workshop_mBot_equipment_one'],
   },
   {
-    title: 'Lær å designe 3D-figurer til 3D-printing',
-    room: 'Hangout 2. etasje',
-    instructors: ['Joachim Haagen Skeie'],
-    checkInLink: ',',
-    description: [
-      'Kom å delta på workshop med 3D-printing! Lær å designe egne figurer i 3D-verktøyet Tinkercad, som kan printes ut på en av 3D-printerne. Vi tar med flere fargevalg på filamenter, slik at du kan velge farger selv!',
-      'I tillegg til Tinkercad, kan du også bruke et kloss-bygge-program for å bygge figurer i en 3D-verden som ligner litt på Minecraft!',
+    title: 'kids.workshop_3Dprinting_title',
+    room: 'kids.workshop_3Dprinting_room',
+    instructors: ['kids.workshop_3Dprinting_instructor_one'],
+    checkInLink: 'kids.workshop_3Dprinting_check_in_link',
+    description: ['kids.workshop_3Dprinting_description_one', 'kids.workshop_3Dprinting_description_two'],
+    info: [
+      'kids.workshop_3Dprinting_info_one',
+      'kids.workshop_3Dprinting_info_two',
+      'kids.workshop_3Dprinting_info_three',
     ],
-    info: ['Maks deltagere: 12', 'Alder: 10+ år', 'Språk: Norsk'],
-    equipment: ['Egen PC eller Mac'],
+    equipment: ['kids.workshop_3Dprinting_equipment_one'],
   },
 ];
 
@@ -57,51 +59,34 @@ const SpeakerKids = () => {
 
   return (
     <ScreenTemplate pageTitle={t('kids.title')}>
-      <Text style={[Assets.styles.sectionSubTitle, { marginTop: -10, fontWeight: '500' }]}>
-        En smakebit på voksenlivet
-      </Text>
+      <Text style={[Assets.styles.sectionSubTitle, { marginTop: -10, fontWeight: '500' }]}>{t('kids.top_text')}</Text>
       <SectionBox sectionTitle={''}>
         <View style={{ marginHorizontal: 'auto', width: '80%' }}>
-          <Text style={[Assets.styles.text]}>
-            JavaZone inviterer alle barn og ungdommer på Rebel søndag 31. august. Vi kjører 3 parallelle workshoper; så
-            det er noe for både barn og ungdommer.
-          </Text>
-          <Text style={[Assets.styles.text, { textAlign: 'center' }]}>Arrangementet er gratis for alle!</Text>
+          <Text style={[Assets.styles.text]}>{t('kids.intro_one')}</Text>
+          <Text style={[Assets.styles.text, { textAlign: 'center' }]}>{t('kids.intro_two')}</Text>
         </View>
         <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{ margin: 10, marginTop: 20 }} />
 
         <View style={{ marginHorizontal: 'auto' }}>
-          <Text style={Assets.styles.sectionSubTitle}>Tid & Sted</Text>
-          <BulletListItem text={'Registrering fra kl. 10.30'} />
-          <BulletListItem text={'Arrangement kl. 11.00 - 15.00'} />
-          <BulletListItem text={'Rebel, Universitetsgata 2'} />
+          <Text style={Assets.styles.sectionSubTitle}>{t('kids.time_and_place')}</Text>
+          <BulletListItem text={t('kids.time_and_place_one')} />
+          <BulletListItem text={t('kids.time_and_place_two')} />
+          <BulletListItem text={t('kids.time_and_place_three')} />
         </View>
         <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{ margin: 10 }} />
 
-        <Text style={Assets.styles.sectionSubTitle}>Påmelding</Text>
-        <Text style={Assets.styles.text}>
-          Påmeldingen åpner mandag 30. juni kl. 12.00. Vi har et begrenset antall plasser, derfor er det viktig at du
-          melder deg på før fristen 10. august kl. 12.00.
-        </Text>
-        <Text style={Assets.styles.text}>
-          Har du ikke mulighet til å komme likevel, er det viktig at dette blir gitt beskjed om så raskt som mulig,
-          siden det pleier å være ventelister på våre kurs.
-        </Text>
+        <Text style={Assets.styles.sectionSubTitle}>{t('kids.sign_up')}</Text>
+        <Text style={Assets.styles.text}>{t('kids.sign_up_description_one')}</Text>
+        <Text style={Assets.styles.text}>{t('kids.sign_up_description_two')}</Text>
 
-        <Text style={Assets.styles.sectionSubTitle}>Utstyr</Text>
-        <Text style={Assets.styles.text}>
-          Kursene krever enten egen laptop eller nettbrett. Mer informasjon finner du på den enkelte workshop.
-        </Text>
+        <Text style={Assets.styles.sectionSubTitle}>{t('kids.equipment')}</Text>
+        <Text style={Assets.styles.text}>{t('kids.equipment_info')}</Text>
 
-        <Text style={Assets.styles.sectionSubTitle}>Tre parallelle workshops</Text>
-        <Text style={Assets.styles.text}>
-          Det er 3 workshoper som går samtidig, så du kan bare være med på en workshop. Les mer om hver av dem under, og
-          meld deg på den det ønsker å delta på. Vi serverer enkel mat underveis. Det er viktig at foreldre/foresatte
-          også setter av tid til å være til stede hele dagen. Vi lager en egen sone for dem med kaffe.
-        </Text>
+        <Text style={Assets.styles.sectionSubTitle}>{t('kids.three_workshops')}</Text>
+        <Text style={Assets.styles.text}>{t('kids.workshops_info')}</Text>
         <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{ margin: 10 }} />
 
-        <Text style={Assets.styles.sectionTitle}>Velkommen til en lærerik dag på Rebel!</Text>
+        <Text style={Assets.styles.sectionTitle}>{t('kids.welcome')}</Text>
       </SectionBox>
       <SvgImage SVG={Assets.UI.DividerDot} height={10} style={{ margin: 10 }} />
 
@@ -124,8 +109,8 @@ const SpeakerKids = () => {
               maxWidth: isMobile ? '100%' : '50%',
               position: 'relative',
               alignItems: 'center',
-              flex: 1, // Ensures all columns grow equally
-              minHeight: 500, // Set to your desired minimum height
+              flex: 1,
+              minHeight: 500,
               marginBottom: 20,
             }}
           >
@@ -144,7 +129,7 @@ const SpeakerKids = () => {
             </Text>
 
             <SectionBox
-              sectionTitle={workshop.title}
+              sectionTitle={t(`${workshop.title}`)}
               style={
                 !isMobile && {
                   height: 1500,
@@ -154,35 +139,38 @@ const SpeakerKids = () => {
                 }
               }
             >
-              <Text style={[Assets.styles.text, { marginHorizontal: 'auto' }]}>Rom: {workshop.room}</Text>
+              <Text style={[Assets.styles.text, { marginHorizontal: 'auto' }]}>Rom: {t(`${workshop.room}`)}</Text>
 
               <Text style={[Assets.styles.sectionSubTitle, { marginTop: 10 }]}>Instruktører</Text>
-              <Text style={[Assets.styles.text, { textAlign: 'center' }]}>{workshop.instructors.join('\n')}</Text>
-
+              {workshop.instructors.map((instructor, i) => (
+                <Text key={i} style={[Assets.styles.text, { textAlign: 'center' }]}>
+                  {t(instructor)}
+                </Text>
+              ))}
               <Text style={[Assets.styles.sectionSubTitle, { marginTop: 20 }]}>Om Workshopen</Text>
               {workshop.description.map((paragraph, i) => (
                 <Text key={i} style={[Assets.styles.text, { marginTop: i === 0 ? 0 : 10 }]}>
-                  {paragraph}
+                  {t(paragraph)}
                 </Text>
               ))}
 
               <Text style={[Assets.styles.sectionSubTitle, { marginTop: 20 }]}>Praktisk info</Text>
               <View style={{ marginHorizontal: 'auto' }}>
                 {workshop.info.map((item, i) => (
-                  <BulletListItem key={i} text={item} />
+                  <BulletListItem key={i} text={t(`${item}`)} />
                 ))}
               </View>
 
               <View style={{ marginHorizontal: 'auto' }}>
                 <Text style={[Assets.styles.sectionSubTitle, { marginTop: 20 }]}>Utstyr</Text>
                 {workshop.equipment.map((item, i) => (
-                  <BulletListItem key={i} text={item} />
+                  <BulletListItem key={i} text={t(`${item}`)} />
                 ))}
               </View>
 
               <LinkButton
                 disabled={true}
-                href={''}
+                href={t(workshop.checkInLink)}
                 title={'Påmelding åpner 30. juni kl. 12.00'}
                 targetBlank={false}
                 margin={isMobile ? 10 : 20}
