7fce43c8530ccfe763dc554cf555a250aa6c0bae
ellaoui94 <abdulquddous.abdellaoui@toll.no>
2025-08-05 19:58:53 +0200

    Update package-lock.json to include new Babel plugins and upgrade ajv to version 8.17.1

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      // Use the standard Reanimated plugin (will be updated when you upgrade)
+      'react-native-reanimated/plugin',
+    ],
   };
 };
